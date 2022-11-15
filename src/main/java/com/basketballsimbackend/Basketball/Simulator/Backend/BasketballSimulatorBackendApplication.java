6fc03ebe4ec5f7ba3bb639d1babcc4ecf2a6acd2
Jacob Nottoli <jacob.nottoli@gmail.com>
2022-11-15 15:24:37 -0600

    first commit

---
+++ src/main/java/com/basketballsimbackend/Basketball/Simulator/Backend/BasketballSimulatorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.basketballsimbackend.Basketball.Simulator.Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballSimulatorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballSimulatorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/basketballsimbackend/Basketball/Simulator/Backend/BasketballSimulatorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basketballsimbackend.Basketball.Simulator.Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballSimulatorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
